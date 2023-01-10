2191100cde5945cb0b98d08d05f3dc2ae3bcd4a5
kingjo1287 <ze.ah1287@gmail.com>
2023-01-11 03:27:21 +0500

    modified

--- C++/Data_Structures/Tree/Binary_Tree.cpp
+++ C++/Data_Structures/Tree/Binary_Tree.cpp
@@ -6,33 +6,76 @@ struct Node{
     Node* left;
     Node* right;
 };
+Node* root;
 
-Node* create(){
-
-    int x;
-    cout<<"Enter the value: "<<endl;
-    cin>> x;
-
-    if(x==-1){
-        return 0;
+Node *new_node(int data){
+    Node *new_node = new Node();
+    new_node->data = data;
+    new_node->left = NULL;
+    new_node->right = NULL;
+    return new_node; 
+}
+Node* insert(Node *root,int data){
+    if(root == NULL){
+        root = new_node(data);
     }
-
-    Node* newNode = new Node();
-    newNode->data = x;
-
-    cout<<"enter value for left node: "<<endl;
-    newNode->left = create();
-
-    cout<<"enter value for right node: "<<endl;
-    newNode->right = create();
-
-    return newNode;
-
+    else if(data<=root->data){
+        root->left = insert(root->left,data);
+    }
+    else{
+        root->right = insert(root->right,data);
+    }
+    return root;
 }
 
-
+bool replace(Node* root,int data,int replce){
+    if(root==NULL){
+        return false;
+    }
+    else if(data == root->data){
+        root->data = replce;
+        return true;
+    }
+    else if(data<=root->data){
+        return replace(root->left,data,replce);
+    }
+    else{
+        return replace(root->right,data,replce);
+    }
+}
+ 
+void inorder(Node *root){
+    if(root==NULL) return;
+    
+    inorder(root->left);
+    cout<<root->data;
+    cout<<" ";
+    inorder(root->right);
+}
+void preorder(Node *root){
+    if(root==NULL) return;
+    cout<<root->data;
+    cout<<" ";
+    preorder(root->left);
+    preorder(root->right);
+}
+void postorder(Node *root){
+    if(root==NULL) return;
+    
+    postorder(root->left);
+    postorder(root->right);
+     cout<<root->data<<"";
+     cout<<" ";
+}
 int main(){
-Node* root;
-root = 0;
-root = create();
+root = NULL;
+root=insert(root,4);
+root=insert(root,57);
+root=insert(root,23);
+root=insert(root,2);
+inorder(root);
+replace(root,57,9);
+inorder(root);
+return 0;
+
 }
